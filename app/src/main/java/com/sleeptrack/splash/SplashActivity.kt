38e360b7de7ea9524328afb99832015ec8692ad8
Rashad <rashadabdulrasheed601@gmail.com>
2022-11-12 16:35:37 +0000

    working

---
+++ app/src/main/java/com/sleeptrack/splash/SplashActivity.kt
@@ -0,0 +1,55 @@
+package com.sleeptrack.splash
+
+import android.animation.ObjectAnimator
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.sleeptrack.R
+import com.sleeptrack.main.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+
+        }
+
+        fader()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            try {
+                val myIntent = Intent(this, MainActivity::class.java)
+                myIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+                this.startActivity(myIntent)
+                overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_left)
+                this.finish()
+            }catch (e : Exception){
+                Log.v("Splash",e.toString())
+            }
+        }, 2500)
+    }
+
+    // value range from [0 - 1] [invisible - visible]
+    private fun fader() {
+        val animator : ObjectAnimator = ObjectAnimator.ofFloat(
+            findViewById(R.id.welcomeText), View.ALPHA,0f,1f)
+        animator.duration = 1000
+        animator.start()
+    }
+}
