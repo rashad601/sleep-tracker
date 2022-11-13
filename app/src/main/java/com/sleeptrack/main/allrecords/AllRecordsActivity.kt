1911f98cdaffcc3a0b11d685561912eed5a2000e
Rashad <rashadabdulrasheed601@gmail.com>
2022-11-13 02:29:14 +0000

    working on it

---
+++ app/src/main/java/com/sleeptrack/main/allrecords/AllRecordsActivity.kt
@@ -0,0 +1,12 @@
+package com.sleeptrack.main.allrecords
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.sleeptrack.R
+
+class AllRecordsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_all_records)
+    }
+}
